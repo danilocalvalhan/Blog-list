44f32fed2179df2df0d48aa984c947d951e695cf
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2023-01-03 19:24:58 -0300

    4.2 Blog list, step2

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.set("strictQuery", true);
+mongoose
+	.connect(mongoUrl)
+	.then(() => {
+		logger.info("Connect to MongoDB");
+	})
+	.catch((error) => {
+		logger.error("Error connecting to MongoDB:", error.message);
+	});
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
