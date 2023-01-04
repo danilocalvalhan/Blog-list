8d96bf30f48c1dfe4c4823a0a152429da3b7b170
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2023-01-04 12:57:26 -0300

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,4 +6,20 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((acc, post) => acc + post.likes, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+	if (blogs.length === 0) {return undefined}
+
+	let mostLikedBlog = blogs[0];
+	for (let i = 1; i < blogs.length; i++) {
+		if (mostLikedBlog.likes < blogs[i].likes) {
+			mostLikedBlog = blogs[i];
+		}
+	}
+	return {
+		title: mostLikedBlog.title,
+		author: mostLikedBlog.author,
+		likes: mostLikedBlog.likes,
+	};
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
